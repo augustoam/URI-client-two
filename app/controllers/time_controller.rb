df2cc3d7e91e6d73d1c208c45979ac8256a05e3c
jeancaio <jeanandreolla@hotmail.com>
2017-12-03 15:40:40 -0200

    bla

--- app/controllers/time_controller.rb
+++ app/controllers/time_controller.rb
@@ -2,4 +2,10 @@ class TimeController < ActionController::API
   def get_time
     render json: {time: Time.now}
   end
+
+  def set_time
+    # debugger
+    Timer.create!(time: params[:time])
+    render json: :ok, status: :ok
+  end
 end
